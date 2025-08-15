b6167f13ec9eaa9e67f2911eebfab8bc45ab264d
Goldn7799 <syeifsultan@gmail.com>
2025-08-16 00:45:26 +0700

    gila

--- modules/main.js
+++ modules/main.js
@@ -1,1 +1,19 @@
 import { animate, utils, onScroll } from './anime.esm.js';
+
+onload = () => {
+  setTimeout(() => {
+    document.querySelectorAll('.cube')[1].classList.remove('opanone')
+    setTimeout(() => {
+      document.querySelectorAll('.cube')[2].classList.remove('opanone')
+      setTimeout(() => {
+        document.querySelectorAll('.cube')[0].classList.remove('opanone')
+        setTimeout(() => {
+          document.querySelector('.loading-screen').classList.add('opanone')
+          setTimeout(() => {
+            document.querySelector('.loading-screen').classList.add('d-none')
+          }, 500);
+        }, 4000);
+      }, 1000);
+    }, 2000);
+  }, 800);
+}
