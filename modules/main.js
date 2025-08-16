0c11ac565665a83f9be27ead304e14964be9fb28
Goldn7799 <syeifsultan@gmail.com>
2025-08-16 14:13:24 +0700

    Song

--- modules/main.js
+++ modules/main.js
@@ -16,12 +16,207 @@
   * @license All_Rights_Reserved
   * @copyright (c) 2025 Syeif Sultoni Akbar
  */
-import { animate, utils, onScroll } from './anime.esm.js';
+import { animate, utils, onScroll, stagger, text } from './anime.esm.js';
+const debug = true
+
+const [ container ] = utils.$('body')
+
+animate('.dynamicisland', {
+  autoplay: onScroll({
+    container, 
+    debug, 
+    target: '.asu',
+    onLeaveBackward: () => {
+      animate('.dynamicisland', {
+        width: '200px',
+        height: '35px',
+        backgroundColor: '',
+        borderRadius: '10px',
+        backdropFilter: '',
+        delay: 100,
+        duration: 250,
+        ease: 'inOut',
+      })
+      animate('.dynamicisland', {
+        opacity: 1,
+        duration: 0,
+        delay: 0,
+        ease: 'inOut'
+      })
+      animate('.diItem', {
+        opacity: 1,
+        delay: 350,
+        duration: 250,
+        ease: 'inOut',
+        autoplay: true
+      })
+      animate('.dynamic-nav .contents', {
+        display: 'none',
+        opacity: 0,
+        delay: 0,
+        duration: 250,
+        ease: 'inOut'
+      })
+    },
+    onEnterForward: () => {
+      animate('.dynamicisland', {
+        width: '95%',
+        height: '60px',
+        backgroundColor: 'rgba(0, 0, 255, 0.4)',
+        borderRadius: '5px',
+        backdropFilter: 'blur(10px)',
+        delay: 100,
+        duration: 250,
+        ease: 'inOut'
+      })
+      animate('.dynamicisland', {
+        opacity: 0,
+        duration: 250,
+        delay: 250,
+        ease: 'inOut'
+      })
+      animate('.diItem', {
+        opacity: 0,
+        delay: 0,
+        duration: 250,
+        ease: 'inOut',
+        autoplay: true
+      })
+      animate('.dynamic-nav .contents', {
+        display: 'flex',
+        opacity: 1,
+        delay: 250,
+        duration: 250,
+        ease: 'inOut'
+      })
+    }
+  })
+})
+
+
+
+let morphDynamic = false
+function morphDynamicIsland() {
+  if (morphDynamic) {
+    morphDynamic = false;
+    animate('.dynamicisland', {
+      width: '200px',
+      height: '35px',
+      backgroundColor: '',
+      borderRadius: '10px',
+      backdropFilter: '',
+      delay: 100,
+      duration: 250,
+      ease: 'inOut'
+    })
+  } else {
+    morphDynamic = true;
+    animate('.dynamicisland', {
+      width: '95%',
+      height: '60px',
+      backgroundColor: 'rgba(0, 0, 255, 0.4)',
+      borderRadius: '5px',
+      backdropFilter: 'blur(10px)',
+      delay: 100,
+      duration: 250,
+      ease: 'inOut'
+    })
+  }
+}
+
+function loadDynamicIsland() {
+  animate('.dynamicisland', {
+    keyframes: {
+      '0%' : {
+        y: '-150%',
+        width: '30px',
+        height: '30px',
+        borderRadius: '50%',
+      },
+      '45%' : {
+        y: ''
+      },
+      '70%' : {
+        width: '30px',
+        height: '30px',
+        borderRadius: '50%'
+      },
+      '76%' : {
+        borderRadius: '10px'
+      },
+      '100%' : {
+        width: '200px',
+        height: '35px'
+      }
+    },
+    delay: 250,
+    duration: 650,
+    ease: 'inExpo',
+    autoplay: true
+  })
+  animate('.diTitem', {
+    opacity: 1,
+    delay: 1000,
+    duration: stagger(250, 50),
+    ease: 'inOut',
+    autoplay: true
+  })
+  animate(document.querySelectorAll('.diTitem')[0], {
+    duration: 500,
+    scale: 1.2,
+    ease: 'outBounce',
+    autoplay: true,
+    loop: true
+  })
+}
+
+function triggerShowSongTitle(title) {
+  console.log(`${title}`)
+  animate('.song-title', {
+    display: 'flex',
+    duration: 0,
+    delay: 0
+  })
+  document.getElementById('songTitleText').innerHTML = `<marquee>${title}</marquee>`
+  const time = title.split('').length * 85
+  animate('.song-title', {
+    opacity: 1,
+    duration: 500,
+    delay: 100
+  })
+  setTimeout(() => {
+    animate('.song-title', {
+      opacity: 0,
+      display: 'none',
+      duration: 500,
+      delay: 0
+    })
+    animate('.song-title', {
+      display: 'flex',
+      duration: 0,
+      delay: 500
+    })
+  }, 2450 + time);
+}
 
 /**
  * Provide Loading Function on website loaded.
  */
 onload = () => {
+  animate('.dynamicisland', {
+    y: '-150%',
+    width: '30px',
+    height: '30px',
+    borderRadius: '50%',
+    delay: 0,
+    duration: 0
+  })
+  if (debug) {
+    loadDynamicIsland()
+    console.log('debug enabled')
+    document.querySelector('.loading-screen').classList.add('d-none')
+    return;
+  };
   setTimeout(() => {
     document.querySelectorAll('.cube')[1].classList.remove('opanone')
     setTimeout(() => {
@@ -32,12 +227,17 @@ onload = () => {
           document.querySelector('.loading-screen').classList.add('opanone')
           setTimeout(() => {
             document.querySelector('.loading-screen').classList.add('d-none')
+            loadDynamicIsland()
           }, 500);
         }, 4000);
       }, 1000);
     }, 2000);
   }, 800);
 }
+
+// setInterval(() => {
+//   console.log(window.scrollY)
+// }, 100);
 
 
 const navCollapseBtn = document.querySelector('.buttons-navcollapse')
