be2f11c2e7f6cb91584b8a9d7066399dbb3e0e78
Goldn7799 <syeifsultan@gmail.com>
2025-08-16 08:48:41 +0700

    Done Navbar 50%

--- modules/main.js
+++ modules/main.js
@@ -1,3 +1,20 @@
+/**
+  * @license
+  * Copyright 2025 SGStudio Under Syeif Sultoni Akbar
+  * 
+  * Licensed under GNU General Public License Version 3 (the "License")
+  * For more information on this, see
+  * 
+  *  https://www.gnu.org/licenses/
+  * 
+  * To "modify" a work means to copy from or adapt all or part of the work
+  * in a fashion requiring copyright permission, other than the making of an
+  * exact copy.  The resulting work is called a "modified version" of the
+  * earlier work or a work "based on" the earlier work.
+  * @author Syeif Sultoni Akbar
+  * @license MIT
+  * @copyright (c) 2025 Syeif Sultoni Akbar
+ */
 import { animate, utils, onScroll } from './anime.esm.js';
 
 onload = () => {
@@ -17,3 +34,16 @@ onload = () => {
     }, 2000);
   }, 800);
 }
+
+const navCollapseBtn = document.querySelector('.buttons-navcollapse')
+function toggleNavside(forced) {
+  if (forced) return () => {
+    navCollapseBtn.classList.remove('active')
+    document.querySelector('.navside').classList.add('hidenavside')
+  }
+  navCollapseBtn.classList.toggle('active')
+  document.querySelector('.navside').classList.toggle('hidenavside')
+}
+navCollapseBtn.addEventListener('click', () => {
+  toggleNavside()
+})
